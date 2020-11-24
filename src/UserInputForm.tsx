71e60a0228a042db895111ebbb32e9adce3938d5
Alex <kavanokami@gmail.com>
2020-11-24 13:44:39 +0400

    feat: add repo title to form

--- src/UserInputForm.tsx
+++ src/UserInputForm.tsx
@@ -2,16 +2,25 @@ import React from 'react';
 
 const UserInputForm = () => {
   let [user, setUser] = React.useState('');
+  let [repo, setRepo] = React.useState('');
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name } = e.target;
     const { value } = e.target;
 
-    setUser(value);
+    if (name === 'userName') {
+      setUser(value);
+    } else {
+      setRepo(value);
+    }
   };
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    console.log(user);
+
+    fetch(`https://api.github.com/repos/${user}/${repo}/pulls?state=all`)
+      .then((res) => res.json())
+      .then((data) => console.log(data));
   };
 
   return (
@@ -22,6 +31,14 @@ const UserInputForm = () => {
         name='userName'
         id='userName'
         value={user}
+        onChange={handleChange}
+      />
+      <label htmlFor='repoe'>Please type a repo title</label>
+      <input
+        type='text'
+        name='repo'
+        id='repo'
+        value={repo}
         onChange={handleChange}
       />
       <button type='submit'>Submit</button>
